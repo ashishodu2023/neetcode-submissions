13e01569272b1600956881cc9ef5dfc78dbccb78
Ashish Verma <143383378+ashishodu2023@users.noreply.github.com>
2026-06-07 00:39:01 -0700

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+
+        if not position or not speed:
+            return 0 
+        
+        max_time = 0 
+        car_fleet = 0 
+
+        tuple_car = sorted(zip(position,speed),reverse=True)
+
+        for pos,spd in tuple_car:
+
+            time = (target - pos) / spd
+
+            if time>max_time:
+                car_fleet+=1
+
+                max_time = time
+
+        return car_fleet
+        
