03ff06628fd1d25638ee8532310c1e6ebe71f0c5
Ashish Verma <143383378+ashishodu2023@users.noreply.github.com>
2026-06-27 15:38:11 -0700

    Add: valid-sudoku - submission-10

---
+++ Data Structures & Algorithms/valid-sudoku/submission-10.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+
+        if not board:
+            return False
+
+        seen = set()
+
+        for i in range(9):
+            for j in range(9):
+                value = board[i][j]
+                if value == ".":
+                    continue
+
+                row_id = f"row{i}{value}"
+                col_id = f"col{j}{value}"
+                box_id = f"box_d{i // 3}{j // 3}{value}"
+
+                if row_id in seen or col_id in seen or box_id in seen:
+                    return False
+
+                seen.add(row_id)
+                seen.add(col_id)
+                seen.add(box_id)
+
+        return True
