7b08213407c23b8ee23f99afda6fb3431d2a6154
Ashish Verma <143383378+ashishodu2023@users.noreply.github.com>
2026-07-09 23:37:22 -0700

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.py
@@ -0,0 +1,32 @@
+class TimeMap:
+    def __init__(self):
+        self.store = dict()
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.store:
+            self.store[key] = []
+
+        self.store[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.store:
+            return ""
+
+        arr = self.store[key]
+        left = 0
+        right = len(arr) - 1
+        result = ""
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if arr[mid][0] == timestamp:
+                    return arr[mid][1]
+
+            if arr[mid][0] <= timestamp:
+                result = arr[mid][1]
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return result
