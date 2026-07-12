9ee829e1fee9da77734c375bc1a1a9bb05a11c5e
Ashish Verma <143383378+ashishodu2023@users.noreply.github.com>
2026-07-12 01:40:20 -0700

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        if not nums:
+            return []
+
+        seen = dict()
+
+        for index, num in enumerate(nums):
+            compliment = target - num
+            if compliment in seen:
+                return [seen[compliment], index]
+
+            seen[num] = index
